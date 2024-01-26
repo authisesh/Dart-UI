caa8d6385f69692b4a8f7317e0b5d6b0c0f579c2
authisesh <authis@eshci.com>
2024-01-26 14:03:38 +0000

    Adding url

--- src/config.tsx
+++ src/config.tsx
@@ -1,3 +1,4 @@
 export const Config = {
-    VITE_API_URL: import.meta.env.VITE_API_URL ?? "http://localhost:8082",
+   // VITE_API_URL: import.meta.env.VITE_API_URL ?? "http://localhost:8082",
+     VITE_API_URL: "http://51.89.164.254:8082",
 }
