ceae9e54c5650c49c278ef018316cacbe36bf964
Kavan Shah <142608409+kavaneshci@users.noreply.github.com>
2024-02-21 10:27:03 +0000

    Update Login.jsx
    Update Header

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -102,7 +102,7 @@ const Login = () => {
               )}
             </Stack>
             <Stack>
-              <Typography variant="h5">Welcome to DART</Typography>
+              <Typography variant="h5">Welcome to DART !! </Typography>
             </Stack>
             <Stack gap={2}>
               <FormControl>
